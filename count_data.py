db83ba725847d9df28dfa725ca82cc43aa3805e2
gamzattirev <fmariko1008@gmail.com>
2021-03-12 14:45:49 +0900

    add program for adjisting data

--- count_data.py
+++ count_data.py
@@ -1,14 +1,15 @@
-INPUT_FILE='/Users/marikof/Documents/research/CTI/SQuAD/train.json'
-KEYWORD1='"is_impossible": "false"'
-KEYWORD2='"is_impossible": "true"'
+#INPUT_FILE='/Users/marikof/Documents/research/CTI/SQuAD/train.json'
+INPUT_FILE='/Users/marikof/Documents/research/CTI/squad_org/train-v2.0.json'
+KEYWORD1='"is_impossible": false'
+KEYWORD2='"is_impossible": true'
 
 f = open(INPUT_FILE, 'r')
 data = f.read()
 f.close()
 
-num_impossible=data.count(KEYWORD1)
-num_possible=data.count(KEYWORD2)
+num_possible=data.count(KEYWORD1)
+num_impossible=data.count(KEYWORD2)
 
-print(num_impossible)
 print(num_possible)
-print(str(num_possible/num_impossible))
+print(num_impossible)
+print(str(num_possible/(num_possible+num_impossible)))
---
+++ create_qa_adjust.py
@@ -0,0 +1,171 @@
+import sys
+import json
+import os
+import const
+import re
+import random
+import string
+
+QUESTION_TOOL='What are the tools used in the attack?'
+QUESTION_GROUP='Who is the attack group?'
+
+INPUT_FILE='input/sample_attack_report_raw.txt'
+VER='v2.0'
+LEN_ID=24
+SENTENSE_NUM=3
+MAX_LEN_PARA=1000
+VUL_RATE=0.2
+POSSIBLE_RATE=0.66
+LABEL_TRAIN='train'
+LABEL_VAL='validation'
+SENTENSE_DELIMETER=". "
+
+num_train=100
+dict_root={}
+dict_root['version'] = VER
+
+def get_answers():
+    with open(const.OUT_TOOL_FILE, 'r') as file:
+        tools=file.readlines()
+        return tools
+
+def get_group_answers():
+    with open(const.OUT_GROUP_FILE, 'r') as file:
+        groups=file.readlines()
+        return groups
+
+
+def get_id(num):
+    dat = string.digits + string.ascii_lowercase + string.ascii_uppercase
+    return ''.join([random.choice(dat) for i in range(num)]).lower()
+
+dict_root={}
+dict_root['version']=VER
+dict_root['data']=[]
+
+def create_json(label,num_data):
+    dict_root['data']=[]
+    possible_cnt=0
+    impossible_cnt = 0
+    num_possible = round(num_data * POSSIBLE_RATE)
+    num_impossible = num_data - num_possible
+
+    with open(INPUT_FILE, 'r') as file:
+        for row in file:
+            sentenses=row.split(SENTENSE_DELIMETER)
+            paragraph = ''
+            for sentense in sentenses:
+                if possible_cnt>num_possible and impossible_cnt>num_impossible:
+                    return
+
+                sentense=sentense+SENTENSE_DELIMETER
+                paragraph = paragraph + sentense
+                if len(paragraph)<MAX_LEN_PARA:
+                    continue
+
+                qas = []
+                dict_data={}
+                dict_data['title']=paragraph.split(' ')[0]
+                dict_data['paragraphs']=[]
+                dict = {}
+                dict_qa = {}
+                dict_qa['question']=QUESTION_TOOL
+                dict_qa['id'] = get_id(LEN_ID)
+                dict_qa['answers'] =[]
+                is_impossible='true'
+
+                # find tools
+                for tool in tools:
+                    tool=tool.replace(const.NEWLINE, "")
+
+                    pattern1 = re.compile(r'(\s+|^){0}(\s+|,|\.)' .format(tool), re.IGNORECASE)
+                    result=pattern1.finditer(paragraph)
+                    for m in result:
+                        dict_answers={}
+                        dict_answers['text']=tool
+                        dict_answers['answer_start'] =m.start()
+                        dict_qa['answers'].append(dict_answers)
+
+                    if len(dict_qa['answers']) > 0:
+                        is_impossible = 'false'
+                        possible_cnt=possible_cnt+1
+                    else:
+                        impossible_cnt=impossible_cnt+1
+
+                    dict_qa['is_impossible'] = is_impossible
+
+                dict['qas'] = qas
+
+                # find groups
+                is_impossible = 'true'
+                dict_qa2=dict_qa.copy()
+
+                dict_qa2['question'] = QUESTION_GROUP
+                dict_qa2['id'] = get_id(LEN_ID)
+                dict_qa2['answers'] = []
+
+                for group in groups:
+                    group=group.replace(const.NEWLINE, "")
+
+                    pattern1 = re.compile(r'(\s+|^){0}(\s+|,|\.)' .format(group), re.IGNORECASE)
+                    result=pattern1.finditer(paragraph)
+                    for m in result:
+                        dict_answers={}
+                        dict_answers['text']=group
+                        dict_answers['answer_start'] =m.start()
+                        dict_qa2['answers'].append(dict_answers)
+
+                if len(dict_qa2['answers']) > 0:
+                    is_impossible = 'false'
+                    possible_cnt = possible_cnt + 1
+                else:
+                    impossible_cnt=impossible_cnt+1
+
+                dict_qa2['is_impossible'] = is_impossible
+
+                if (possible_cnt < num_possible and dict_qa['is_impossible'] == 'false') \
+                        or (impossible_cnt < num_impossible and dict_qa['is_impossible'] == 'true'):
+                    dict['qas'].append(dict_qa)
+
+                if (possible_cnt < num_possible and dict_qa2['is_impossible'] == 'false') \
+                            or (impossible_cnt < num_impossible and dict_qa2['is_impossible'] == 'true'):
+                    dict['qas'].append(dict_qa2)
+
+                dict['context']=paragraph
+                dict_data['paragraphs'].append(dict)
+
+
+                dict_root['data'].append(dict_data)
+
+                paragraph = ''
+
+if len(sys.argv)>0:
+    num_train  = int(sys.argv[1])
+
+num_val=round(num_train*VUL_RATE)
+
+
+tools=get_answers()
+groups=get_group_answers()
+
+create_json(LABEL_TRAIN, num_train)
+
+OUT_FILE=LABEL_TRAIN+'.json'
+
+with open(OUT_FILE, "w", encoding='utf8') as out:
+    json.dump(dict_root,out)
+
+create_json(LABEL_VAL, num_val)
+
+OUT_FILE=LABEL_VAL+'.json'
+
+with open(OUT_FILE, "w", encoding='utf8') as out:
+    json.dump(dict_root,out)
+
+
+
+
+
+
+
+
